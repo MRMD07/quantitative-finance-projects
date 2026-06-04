8b4c2c5d14de094c2c2307de5435171d83f50a05
Muhammad Dayyan Tipu <dayyantipu@gmail.com>
2026-06-04 19:57:53 +0500

    Update analysis.py

--- KSE-100 Analysis/analysis.py
+++ KSE-100 Analysis/analysis.py
@@ -1,2 +1,11 @@
+from ast import And
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-# force update
+data = pd.read_csv("Karachi 100 Historical Data.csv")
+data['Price'] = data['Price'].str.replace(',','',regex = False).astype(float)
+data['Date'] = pd.to_datetime(data['Date'])
+data.sort_values(by = 'Date', ascending= True, inplace = True)
+data.reset_index(drop = True, inplace = True)
