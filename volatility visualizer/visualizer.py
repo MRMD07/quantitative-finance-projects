231f82f5fb062b11f033367965b15324d884831d
Muhammad Dayyan Tipu <dayyantipu@gmail.com>
2026-09-03 19:18:36 +0500

    Update visualizer.py

--- volatility visualizer/visualizer.py
+++ volatility visualizer/visualizer.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
+from scipy.optimize import brentq
 
 tk= yf.Ticker('AAPL')
 
@@ -19,4 +21,15 @@ for date in tk.options:
 puts_df = pd.concat(puts, ignore_index = True)
 calls_df = pd.concat(calls, ignore_index= True)
 
+Spot = puts
 
+def black_scholes_call(S, K, T, r, sigma):
+    # Calculates the theoretical call price
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    return S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+
+def find_volatility(market_price, S, K, T, r):
+    # Objective function: difference between model price and target market price
+    def objective(sigma):
+        return black_scholes_call(S, K, T, r, sigma) - market_price
